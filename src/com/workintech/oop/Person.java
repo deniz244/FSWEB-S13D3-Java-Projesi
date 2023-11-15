6dfcc65a4bb44905ecddc0e9c3548116e5ecafc1
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-15 12:20:47 +0300

    Person class completed - chaining problem

---
+++ src/com/workintech/oop/Person.java
@@ -0,0 +1,50 @@
+package com.workintech.oop;
+
+public class Person {
+
+    //instance variables/fields
+    String firstName;
+    String lastName;
+    int age;
+    String gender;
+    String address;
+    String phoneNum;
+
+    //Constructor1
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+
+    //Constructor2
+    public Person(String gender,String address,String phoneNum){
+        //this(firstName,lastName,age);
+        this.gender=gender;
+        this.address=address;
+        this.phoneNum=phoneNum;
+    }
+
+    //Methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public boolean isTeen(){
+        return age >= 13 && age <= 19;
+    }
+
+
+
+
+
+}
