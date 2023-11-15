72f27987696b97de5f3f5e16a22a1b3cc0a4d045
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-15 12:33:20 +0300

    Wall class completed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import com.workintech.oop.Person;
+import com.workintech.oop.Wall;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,20 @@ public class Main {
         System.out.println("Age: " + person.getAge());
 
         System.out.println("isTeen: " + person.isTeen());
+
+        System.out.println("--------------------------------");
+
+        Wall wall = new Wall(5,4);
+
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
 
     }
 }
--- src/com/workintech/oop/Wall.java
+++ src/com/workintech/oop/Wall.java
@@ -1,4 +1,44 @@
 package com.workintech.oop;
 
 public class Wall {
+    //fields
+    double width;
+    double height;
+
+    //constructor
+    public Wall(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    //methods
+    public double getWidth(){
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public void setWidth(double param){
+        if(param < 0){
+            width = 0;
+        }else {
+            width = param;
+        }
+    }
+    public void setHeight(double param){
+        if(param < 0){
+            height = 0;
+        }else {
+            height = param;
+        }
+    }
+
+    public double getArea(){
+        return width*height;
+    }
+
+
+
 }
